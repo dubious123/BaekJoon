1b291084a8877265d64c707f65c0651cba1d055f
dubious123 <62788448+dubious123@users.noreply.github.com>
2021-07-11 01:23:23 +0900

    ~Lv5_7

--- BaekJoon/Lv.5/Program.cs
+++ BaekJoon/Lv.5/Program.cs
@@ -6,13 +6,13 @@ namespace Lv._5
     {
         static void Main(string[] args)
         {
-            //_10818 Lv4_1 = new();
-            //_2562 Lv4_2 = new();
-            //_2577 Lv4_3 = new();
-            //_3052 Lv4_4 = new();
-            //_1546 Lv4_5 = new();
-            // _8958 Lv4_6 = new();
-            _4344 Lv4_7 = new();
+            //_10818 Lv5_1 = new();
+            //_2562 Lv5_2 = new();
+            //_2577 Lv5_3 = new();
+            //_3052 Lv5_4 = new();
+            //_1546 Lv5_5 = new();
+            // _8958 Lv5_6 = new();
+            _4344 Lv5_7 = new();
         }
     }
 }
